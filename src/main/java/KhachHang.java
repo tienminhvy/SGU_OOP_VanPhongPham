3ef3bca223b738751f7d73a5339c93b071491d92
Tiền Minh Vy <tienminhvydev@gmail.com>
2022-11-01 11:13:31 +0700

    Thêm code vào class KhachHang

--- src/main/java/KhachHang.java
+++ src/main/java/KhachHang.java
@@ -1,12 +1,66 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 /**
  *
- * @author Admin
+ * @author Tien Minh Vy
  */
-public class KhachHang {
+import java.util.Scanner;
+public class KhachHang extends Nguoi {
+    private int soDonHangDaThanhToan;
+    private int tongTienDaThanhToan;
+    private int maKhachHang;
+    public static Scanner sc = new Scanner(System.in);
+
+    public KhachHang() {
+    }
+
+    public KhachHang(int soDonHangDaThanhToan, int tongTienDaThanhToan, int maKhachHang) {
+        this.soDonHangDaThanhToan = soDonHangDaThanhToan;
+        this.tongTienDaThanhToan = tongTienDaThanhToan;
+        this.maKhachHang = maKhachHang;
+    }
+
+    public KhachHang(int soDonHangDaThanhToan, int tongTienDaThanhToan, int maKhachHang, String hoTen, String ngaySinh, String cmt, String diaChi, String soDienThoai) {
+        super(hoTen, ngaySinh, cmt, diaChi, soDienThoai);
+        this.soDonHangDaThanhToan = soDonHangDaThanhToan;
+        this.tongTienDaThanhToan = tongTienDaThanhToan;
+        this.maKhachHang = maKhachHang;
+    }
+
+    public int getSoDonHangDaThanhToan() {
+        return soDonHangDaThanhToan;
+    }
+
+    public void setSoDonHangDaThanhToan(int soDonHangDaThanhToan) {
+        this.soDonHangDaThanhToan = soDonHangDaThanhToan;
+    }
+
+    public int getTongTienDaThanhToan() {
+        return tongTienDaThanhToan;
+    }
+
+    public void setTongTienDaThanhToan(int tongTienDaThanhToan) {
+        this.tongTienDaThanhToan = tongTienDaThanhToan;
+    }
+
+    public int getMaKhachHang() {
+        return maKhachHang;
+    }
+
+    public void setMaKhachHang(String maKhachHang) {
+        this.maKhachHang = Integer.parseInt(maKhachHang);
+    }
+
+    @Override
+    public void nhap() {
+        System.out.print("Nhap ma khach hang: ");
+        setMaKhachHang(sc.nextLine());
+        super.nhap(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+    @Override
+    public void xuat() {
+        System.out.println("Ma khach hang: "+getMaKhachHang());
+        super.xuat(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+    
     
 }
